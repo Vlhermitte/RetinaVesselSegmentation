6412c5345eebf7156befd56ebd6023eb8f368075
Vlhermitte <valentin.lhermitte2604@gmail.com>
2024-04-04 09:12:43 +0200

    Update model

--- loss.py
+++ loss.py
@@ -6,11 +6,9 @@ class DiceLoss(torch.nn.Module):
         self.smooth = 1.0
 
     def forward(self, y_pred, y_true):
-        assert y_pred.size() == y_true.size()
+        assert y_pred.size() == y_true.size(), f"y_pred: {y_pred.size()}, y_true: {y_true.size()}"
         y_pred = y_pred[:, 0].contiguous().view(-1)
         y_true = y_true[:, 0].contiguous().view(-1)
         intersection = (y_pred * y_true).sum()
-        dsc = (2. * intersection + self.smooth) / (
-                y_pred.sum() + y_true.sum() + self.smooth
-        )
+        dsc = (2. * intersection + self.smooth) / (y_pred.sum() + y_true.sum() + self.smooth)
         return 1. - dsc
